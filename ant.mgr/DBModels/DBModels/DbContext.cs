7e720b07c47764bb775a5dcaa9d7db918a8bed63
1877682825@qq.com <yuzd>
2019-07-05 07:23:46 +0800

    Debug的时候增加sql的打印

--- ant.mgr/DBModels/DBModels/DbContext.cs
+++ ant.mgr/DBModels/DBModels/DbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Infrastructure.Logging;
 using Infrastructure.Web;
 
 namespace DbModel
@@ -13,8 +14,10 @@ namespace DbModel
             get
             {
                 var db = new MysqlDbContext<AntEntity>("ant_mysql");
+#if DEBUG
                 db.IsEnableLogTrace = true;
                 db.OnLogTrace = OnCustomerTraceConnection;
+#endif
                 return db;
             }
         }
@@ -23,23 +26,37 @@ namespace DbModel
         /// 记录sql
         /// </summary>
         /// <param name="customerTraceInfo"></param>
-        public static void OnCustomerTraceConnection(CustomerTraceInfo customerTraceInfo)
+        private static void OnCustomerTraceConnection(CustomerTraceInfo customerTraceInfo)
         {
-            string sql = customerTraceInfo.SqlText;
             try
             {
-                sql = customerTraceInfo.CustomerParams.Aggregate(customerTraceInfo.SqlText,
-                    (current, item) => current.Replace(item.Key, item.Value.Value.ToString()));
+                string sql = Environment.NewLine + "#####################################################" + Environment.NewLine + customerTraceInfo.SqlText;
+                try
+                {
+                    sql = customerTraceInfo.CustomerParams.Aggregate(customerTraceInfo.SqlText,
+                        (current, item) => current.Replace(item.Key, item.Value.Value.ToString()));
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+                sql += Environment.NewLine;
+                foreach (var detail in customerTraceInfo.RunTimeList)
+                {
+                    
+                    var sencond = (int)detail.Duration.TotalSeconds;
+                    var time = sencond + "秒";
+                    if (sencond < 1)
+                    {
+                        time = detail.Duration.TotalMilliseconds + "豪秒";
+                    }
+                    sql += $"Server：{detail.Server},DB名称：{detail.DbName}, 执行时间：{time}" +Environment.NewLine + "#####################################################" + Environment.NewLine;
+                    LogHelper.Info("SQL",sql);
+                }
             }
             catch (Exception)
             {
                 //ignore
-            }
-            sql += Environment.NewLine;
-            foreach (var detail in customerTraceInfo.RunTimeList)
-            {
-                sql += $"Server：{detail.Server},DB名称：{detail.DbName}, 执行时间：{detail.Duration.TotalSeconds}秒" +Environment.NewLine ;
-                Debug.Write(sql);
             }
         }
     }
