983eeb201ae900630bf86a79e10f49139dcfb73a
zdyu <zdyu@Ctrip.com>
2019-07-05 14:29:36 +0800

    去掉写死端口

--- ant.mgr/mgr.core/Program.cs
+++ ant.mgr/mgr.core/Program.cs
@@ -12,7 +12,6 @@ namespace ant.mgr.core
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://*:5000");
+                .UseStartup<Startup>();
     }
 }
